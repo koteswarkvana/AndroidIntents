c1112fd148a8a71ef79cd711539f05d0ac54be3c
Koteswarao <kotesh@kvana.com>
2017-11-11 13:37:58 +0530

    intents action created ...

--- app/src/main/java/com/koti/apple/androidintents/MainActivity.java
+++ app/src/main/java/com/koti/apple/androidintents/MainActivity.java
@@ -1,16 +1,46 @@
 package com.koti.apple.androidintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "MainActivity";
+    public static final String MESSAGE  = "MESSAGE";
+    private EditText etrValue;
+    private Button btSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG, "onCreate: intents using ...");
+        etrValue= (EditText) findViewById(R.id.et_value);
+        btSubmit= (Button) findViewById(R.id.btsubmit);
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: button clicked >>> ");
+                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra(MESSAGE, etrValue.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+       /* switch (view.getId()){
+            case R.id.btsubmit :
+                Log.i(TAG, "onClick: button clicked >>> ");
+                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra(MESSAGE, etrValue.getText().toString());
+                break;
+        }*/
+    }
+
 }
---
+++ app/src/main/java/com/koti/apple/androidintents/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.koti.apple.androidintents;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
+    private TextView tv_msg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_screen);
+        Log.e(TAG, "onCreate: intents using ...");
+        String msg=getIntent().getExtras().getString(MainActivity.MESSAGE,"");
+        tv_msg=(TextView) findViewById(R.id.tv_msg);
+        tv_msg.setText(msg);
+    }
+}
