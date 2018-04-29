55ad4ccecb6b7640256390a567e861c007211a05
Koteswarao <kotesh@kvana.com>
2018-04-29 23:20:38 +0530

    send datatype values, array and list data and also create chooser for email.

--- app/src/main/java/com/koti/apple/androidintents/MainActivity.java
+++ app/src/main/java/com/koti/apple/androidintents/MainActivity.java
@@ -8,10 +8,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
-    public static final String MESSAGE = "MESSAGE";
+    public static final String STR_ARRAYLIST = "STR_ARRAYLIST";
+    public static final String STR_ARRAY = "STR_ARRAY";
+    public static final String STR_MESSAGE = "STR_MESSAGE";
+    public static final String INT_VALUE = "INT_VALUE";
+    public static final String FLOAT_VALUE = "FLOAT_VALUE";
+    public static final String DOUBLE_VALUE = "DOUBLE_VALUE";
+    public static final String LONG_VALUE = "LONG_VALUE";
+    public static final String CHAR_VALUE = "CHAR_VALUE";
+    public static final String BOOLEAN_VALUE = "BOOLEAN_VALUE";
+
+    private String[] languageNames = {"objective-c", "swift", "android", "kotlin", "ruby", "sql", "javascript"};
+    private ArrayList<String> androidTopics = new ArrayList<>();
+
     private EditText etrValue;
     private Button btSubmit;
 
@@ -23,6 +38,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etrValue = (EditText) findViewById(R.id.et_value);
         btSubmit = (Button) findViewById(R.id.btsubmit);
         btSubmit.setOnClickListener(this);
+        // Adding elements to the array list
+        androidTopics.add("Activity");
+        androidTopics.add("Fragment");
+        androidTopics.add("Services");
+        androidTopics.add("Content Provider");
+        androidTopics.add("Broadcast Receiver");
+        androidTopics.add("Sqlite Database");
     }
 
     @Override
@@ -31,7 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btsubmit:
                 Log.i(TAG, "onClick: button clicked >>> ");
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                intent.putExtra(MESSAGE, etrValue.getText().toString());
+                intent.putExtra(STR_MESSAGE, etrValue.getText().toString());
+                intent.putExtra(INT_VALUE, 1234);
+                intent.putExtra(FLOAT_VALUE, 123.4);
+                intent.putExtra(DOUBLE_VALUE, 12.34);
+                intent.putExtra(LONG_VALUE, 12345678);
+                intent.putExtra(CHAR_VALUE, 'c');
+                intent.putExtra(BOOLEAN_VALUE, true);
+
+                intent.putExtra(STR_ARRAY, languageNames);
+                intent.putStringArrayListExtra(STR_ARRAYLIST, androidTopics);
                 startActivity(intent);
                 break;
         }
